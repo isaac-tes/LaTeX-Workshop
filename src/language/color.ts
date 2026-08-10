5e82a4563afc426220c5f3fb4498fddfca4c41d2
Jerome Lelong <jerome.lelong@gmail.com>
2026-08-10 09:36:12 +0200

    Complete refactor and many fixes

--- src/language/color.ts
+++ src/language/color.ts
@@ -1,107 +1,88 @@
 
 import * as vscode from 'vscode'
 
-function colorParserCMYK(document: vscode.TextDocument, colors: vscode.ColorInformation[], regex: RegExp) {
-    const text = document.getText()
+function colorParserCMYK(colorAsString: string): vscode.Color | undefined {
 
-    let match: RegExpExecArray | null
-    while ((match = regex.exec(text))) {
-        const start = document.positionAt(match.index + 12)
-        const end = document.positionAt(match.index + match[0].length)
-        const hex = match[3].split(',')
-        const c = parseFloat(hex[0])
-        const m = parseFloat(hex[1])
-        const y = parseFloat(hex[2])
-        const k = parseFloat(hex[3])
-        const r = (1 - c) * (1 - k)
-        const g = (1 - m) * (1 - k)
-        const b = (1 - y) * (1 - k)
-
-        colors.push(
-            new vscode.ColorInformation(
-                new vscode.Range(start, end),
-                new vscode.Color(r, g, b, 1)
-            )
-        )
+    const tokens = colorAsString.split(',')
+    if (tokens.length !== 4) {
+        return undefined
     }
+    const c = parseFloat(tokens[0])
+    const m = parseFloat(tokens[1])
+    const y = parseFloat(tokens[2])
+    const k = parseFloat(tokens[3])
+    const r = (1 - c) * (1 - k)
+    const g = (1 - m) * (1 - k)
+    const b = (1 - y) * (1 - k)
+
+    return new vscode.Color(r, g, b, 1)
 }
 
-function colorParserHTML(document: vscode.TextDocument, colors: vscode.ColorInformation[], regex: RegExp) {
-    const text = document.getText()
-
-    let match: RegExpExecArray | null
-    while ((match = regex.exec(text))) {
-        const start = document.positionAt(match.index + 12)
-        const end = document.positionAt(match.index + match[0].length)
-        const hex = match[3]
-        const r = parseInt(hex.slice(0, 2), 16) / 255
-        const g = parseInt(hex.slice(2, 4), 16) / 255
-        const b = parseInt(hex.slice(4, 6), 16) / 255
-
-        colors.push(
-            new vscode.ColorInformation(
-                new vscode.Range(start, end),
-                new vscode.Color(r, g, b, 1)
-            )
-        )
+function colorParserHTML(colorAsString: string): vscode.Color | undefined {
+    if (colorAsString.length !== 6) {
+        return undefined
     }
+    const r = parseInt(colorAsString.slice(0, 2), 16) / 255
+    const g = parseInt(colorAsString.slice(2, 4), 16) / 255
+    const b = parseInt(colorAsString.slice(4, 6), 16) / 255
+    return new vscode.Color(r, g, b, 1)
 }
 
-function colorParserRGB(document: vscode.TextDocument, colors: vscode.ColorInformation[], regex: RegExp) {
-    const text = document.getText()
-
-    let match: RegExpExecArray | null
-    while ((match = regex.exec(text))) {
-        const start = document.positionAt(match.index + 12)
-        const end = document.positionAt(match.index + match[0].length)
-        const hex = match[3].split(',')
-        const r = parseFloat(hex[0])
-        const g = parseFloat(hex[1])
-        const b = parseFloat(hex[2])
-
-        colors.push(
-            new vscode.ColorInformation(
-                new vscode.Range(start, end),
-                new vscode.Color(r, g, b, 1)
-            )
-        )
+function colorParserRGB(colorAsString: string): vscode.Color | undefined {
+    const tokens = colorAsString.split(',')
+    if (tokens.length !== 3) {
+        return undefined
     }
+    const r = parseFloat(tokens[0])
+    const g = parseFloat(tokens[1])
+    const b = parseFloat(tokens[2])
+
+    return new vscode.Color(r, g, b, 1)
 }
 
-function colorParserGray(document: vscode.TextDocument, colors: vscode.ColorInformation[], regex: RegExp) {
-    const text = document.getText()
-
-    let match: RegExpExecArray | null
-    while ((match = regex.exec(text))) {
-        const start = document.positionAt(match.index + 12)
-        const end = document.positionAt(match.index + match[0].length)
-        const gray = parseFloat(match[3])
-
-        colors.push(
-            new vscode.ColorInformation(
-                new vscode.Range(start, end),
-                new vscode.Color(gray, gray, gray, 1)
-            )
-        )
-    }
+function colorParserGray(colorAsString: string): vscode.Color | undefined{
+    const gray = parseFloat(colorAsString)
+    return new vscode.Color(gray, gray, gray, 1)
 }
 
 export class DocColorProvider implements vscode.DocumentColorProvider {
 
     provideDocumentColors(document: vscode.TextDocument): vscode.ProviderResult<vscode.ColorInformation[]> {
         const colors: vscode.ColorInformation[] = []
+        const text = document.getText()
+        const defineColorRegex = /(\\definecolor\{\w+\}\{(\w+)\}\{)([^}]+)/g
+        let match: RegExpExecArray | null
 
-        // type match?
-        // text as parameter?
-        const handlers = [
-            { 'func': colorParserHTML, 'regex': /(\\definecolor\{(HTML)\}\{)([^}]+)/g },
-            { 'func': colorParserGray, 'regex': /(\\definecolor\{(gray)\}\{)([^}]+)/g },
-            { 'func': colorParserRGB, 'regex': /(\\definecolor\{(rgb)\}\{)([^}]+)/g },
-            { 'func': colorParserCMYK, 'regex': /(\\definecolor\{(cmyk)\}\{)([^}]+)/g },
-        ]
+        while ((match = defineColorRegex.exec(text))) {
+            const start = document.positionAt(match.index + match[1].length)
+            const end = start.translate(0, match[3].length)
+            let color: vscode.Color | undefined = undefined
+            switch(match[2].toLowerCase()) {
+                case 'html':
+                    color = colorParserHTML(match[3])
+                    break
+                case 'gray':
+                    color = colorParserGray(match[3])
+                    break
+                case 'rgb':
+                    color = colorParserRGB(match[3])
+                    break
+                case 'cmyk':
+                    color = colorParserCMYK(match[3])
+                    break
+                default:
+                    break
+            }
+            if (color !== undefined) {
 
-        handlers.forEach((x) => x['func'](document, colors, x['regex']))
-
+                colors.push(
+                    new vscode.ColorInformation(
+                        new vscode.Range(start, end),
+                        color
+                    )
+                )
+            }
+        }
         return colors
     }
 
@@ -111,10 +92,10 @@ export class DocColorProvider implements vscode.DocumentColorProvider {
         let label = ''
         let type = 'UNKNOWN'
 
-        const regex = /\\definecolor\{([a-zA-Z]+)\}/g
+        const regex = /\\definecolor\{\w+\}\{([a-zA-Z]+)\}/g
         const match = regex.exec(line)
         if (match) {
-            type = match[1]
+            type = match[1].toLowerCase()
         }
         else {
             return
@@ -150,7 +131,6 @@ export class DocColorProvider implements vscode.DocumentColorProvider {
                 return
         }
 
-        label = `{${type}}{${label}`
         return [new vscode.ColorPresentation(label)]
     }
 }
